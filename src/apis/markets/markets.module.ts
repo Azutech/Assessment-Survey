df29bcfbac2c644b54b0d14db966950c8d6ab303
Azutech <azunna.onugha@gmail.com>
2026-06-18 18:46:24 +0100

    feat: integrate Mongoose for Market entity and configure database connection

--- src/apis/markets/markets.module.ts
+++ src/apis/markets/markets.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MarketsService } from './markets.service';
 import { MarketsController } from './markets.controller';
+import { Market, MarketSchema } from './entity/market.entity';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 
 @Module({
+  imports: [
+        MongooseModule.forFeature([
+      { name: Market.name, schema: MarketSchema },
+  ])
+],
   controllers: [MarketsController],
   providers: [MarketsService],
 })
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { ConfigModule, ConfigService} from '@nestjs/config'
 import { MarketsModule } from './apis/markets/markets.module';
 import { SurveysModule } from './apis/surveys/surveys.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env', // optional, defaults to process.env
+    }),
+      MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('DATABASE_URL'),
+      }),
     }),
     MarketsModule,
     SurveysModule,
