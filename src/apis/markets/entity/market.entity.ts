81c7d7fe401110627ebeb68213bd991e701ae758
Azutech <azunna.onugha@gmail.com>
2026-06-18 18:27:04 +0100

    feat: add date-fns dependency and implement Market entity with schema and createdAt field

--- src/apis/markets/entity/market.entity.ts
+++ src/apis/markets/entity/market.entity.ts
@@ -1,1 +1,34 @@
-export class Market {}
+// markets/schemas/market.schema.ts
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { startOfSecond } from 'date-fns';
+import { Document } from 'mongoose';
+
+export type MarketDocument = Market & Document;
+
+export class Market {
+  @Prop({ required: true, trim: true })
+  marketName: string;
+
+  @Prop({ required: true, trim: true })
+  LGA: string;
+
+  @Prop({ required: true, trim: true })
+  marketState: string;
+
+  @Prop({ type: String, required: false })
+  marketEntity: string;
+
+  @Prop({ type: String, required: false })
+  marketGPS: string;
+
+  @Prop({ default: true })
+  isActive: boolean;
+
+  @Prop({
+    type: Date,
+    default: () => startOfSecond(new Date()),
+  })
+  createdAt: Date;
+}
+
+export const MarketSchema = SchemaFactory.createForClass(Market);
