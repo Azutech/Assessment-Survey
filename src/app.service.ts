924060903844de9e4750a91f37606cd0fbd9279c
Azutech <azunna.onugha@gmail.com>
2026-06-19 13:24:24 +0100

    Return API for markets

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return `Welcome to the Market Survey API! This API allows you to manage and retrieve information about various markets. You can add new markets, view specific market details, and search for markets based on different criteria. Explore the endpoints to get started!`;
   }
 }
