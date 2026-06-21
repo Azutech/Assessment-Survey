dab1104f2732fe6269a017658806c889443c0601
Azutech <azunna.onugha@gmail.com>
2026-06-21 14:25:47 +0100

    feat: define Admin entity with properties and schema for MongoDB

--- src/apis/admin/entity/admin.entity.ts
+++ src/apis/admin/entity/admin.entity.ts
@@ -1,1 +1,31 @@
-export class Admin {}
+import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { startOfSecond } from 'date-fns';
+import { Document } from 'mongoose';
+@Schema()
+export class Admin extends Document {
+  @Prop({ required: true })
+  fullName: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: false })
+  avatar: string;
+
+  @Prop({ type: String, required: false, default: 'admin' })
+  userType: string;
+
+  @Prop({ type: String, default: 'active' })
+  exportPermission: string;
+
+  @Prop({
+    type: Date,
+    default: () => startOfSecond(new Date()),
+  })
+  createdAt: Date;
+}
+
+export const AdminSchema = SchemaFactory.createForClass(Admin);
